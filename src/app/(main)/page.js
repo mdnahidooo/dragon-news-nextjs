51a519705532863dd3a700179149d4260e899c48
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-25 01:59:32 +0600

    Display Categories & Design Sidebar

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,3 +1,6 @@
+
+import LeftSideBar from "@/components/homepage/news/LeftSideBar";
+import RightSideBar from "@/components/homepage/news/RightSideBar";
 import Image from "next/image";
 
 
@@ -16,20 +19,17 @@ export default async function Home() {
   return (
     <div>
       <div className="w-11/12 mx-auto grid grid-cols-12 gap-4 my-15">
+
         <div className="col-span-3">
-          <h2 className="text-lg font-bold ">All Categories</h2>
-          
-          <ul className="flex flex-col gap-3 mt-6">
-            {
-              categories.news_category.map((category) => <li key={category.category_id} className="bg-slate-100 p-2 rounded-md font-bold text-md text-center">{category.category_name}</li>)
-            }
-          </ul>
+          <LeftSideBar categories={categories} activeId={null}></LeftSideBar>
         </div>
+
         <div className="text-3xl bg-purple-100 font-bold col-span-6">
           All News
         </div>
-        <div className="text-3xl bg-yellow-100 font-bold col-span-3">
-          Social Icons
+
+        <div className="col-span-3">
+          <RightSideBar></RightSideBar>
         </div>
       </div>
     </div>
---
+++ src/components/homepage/news/LeftSideBar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+
+const LeftSideBar = ({ categories, activeId }) => {
+    return (
+        <div>
+            <h2 className="text-lg font-bold ">All Categories</h2>
+
+            <ul className="flex flex-col gap-3 mt-6">
+                {
+                    categories.news_category.map((category) =>
+                        <li key={category.category_id}
+                            className={`${activeId === category.category_id && "bg-[#D72050] text-white"} rounded-md font-bold text-sm text-center`}
+                        >
+                            <Link href={`category/${category.category_id}`} className='block p-2'>
+                                {category.category_name}
+                            </Link>
+                        </li>)
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default LeftSideBar;
