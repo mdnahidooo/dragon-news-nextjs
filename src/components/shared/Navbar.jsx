2fc0db2aeca8c81b2042c1024632d9b8cf194a19
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-26 01:07:27 +0600

    Added Logged In User, Signout, Password Toggle

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,28 +1,59 @@
+"use client"
+
 import Link from 'next/link';
 import React from 'react';
 import userAvatar from '@/assets/user.png'
 import Image from 'next/image';
 import NavLink from './NavLink';
+import { authClient } from '@/lib/auth-client';
 
 const Navbar = () => {
-    
+
+    const { data: session, isPending } = authClient.useSession();
+    const user = session?.user;
+
     return (
-        <div className='w-11/12 mx-auto flex justify-between items-center gap-4 mt-6'>
-            <div>
-                
-            </div>
-            <ul className='flex justify-between items-center gap-3 text-gray-700'>
+        <div className='w-11/12 mx-auto flex flex-col md:flex-row items-center gap-4 mt-6'>
+
+            {/* left */}
+            <div className='flex-1 hidden md:block'></div>
+
+            {/* center nav */}
+            <ul className='flex flex-1 justify-center items-center text-gray-700 gap-3 text-sm md:text-base'>
                 <li><NavLink href={'/'}>Home</NavLink></li>
                 <li><NavLink href={'/about-us'}>About</NavLink></li>
                 <li><NavLink href={'/career'}>Career</NavLink></li>
             </ul>
 
-            <div className='flex items-center gap-2'>
-                <Image src={userAvatar} alt='User Avatar' width={50} height={50}></Image>
-                <button className='btn bg-black text-white'>
-                    <Link href={"/login"}>Login</Link>
-                </button>
-            </div>
+            {/* right */}
+            {isPending ? (
+                <span className="loading loading-spinner loading-lg"></span>
+            ) : user ? (
+                <div className="flex flex-1 justify-center md:justify-end items-center gap-2 text-sm md:text-base">
+                    <h2>Hello, {user.name}</h2>
+
+                    <Image
+                        src={user.image || userAvatar}
+                        alt="User avatar"
+                        width={40}
+                        height={40}
+                        className="md:w-15 md:h-15"
+                    />
+
+                    <button
+                        className="btn bg-[#D72050] text-white btn-sm md:btn-md"
+                        onClick={async () => await authClient.signOut()}
+                    >
+                        <Link href={"/"}>LogOut</Link>
+                    </button>
+                </div>
+            ) : (
+                <div className="flex flex-1 justify-center md:justify-end">
+                    <button className="btn bg-[#D72050] text-white btn-sm md:btn-md">
+                        <Link href={"/login"}>Login</Link>
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
